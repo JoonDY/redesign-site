b57384d305700ed4de4b9355df041eb1f59fce4b
Joon <daniel.joon.yi@gmail.com>
2021-08-01 14:03:28 -0700

    Finish event section and add footer

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav-list');
+const calendarBtn = document.getElementById('calendar-btn');
+const currentEventBtn = document.getElementById('current-event-btn');
+const calendar = document.getElementById('event-calendar');
+const currentEvent = document.getElementById('current-event')
 
 const toggleNav = () => {
   nav.classList.toggle('hide');
 }
 
+const showCalendar = () => {
+  calendar.classList.remove('hide');
+  currentEvent.classList.add('hide');
+}
+
+const showCurrentEvent = () => {
+  calendar.classList.add('hide');
+  currentEvent.classList.remove('hide');
+}
+
 hamburger.addEventListener('click', toggleNav);
+calendarBtn.addEventListener('click', showCalendar);
+currentEventBtn.addEventListener('click', showCurrentEvent);
 
